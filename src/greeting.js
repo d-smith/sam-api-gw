29fddb7a6dfd22ebdc78d944068f030995111334
d-smith <doug.smith.mail@gmail.com>
2019-02-18 08:33:37 -0800

    Updates to map response correctly

--- src/greeting.js
+++ src/greeting.js
@@ -1,14 +1,18 @@
 const greet = async (event, context) => {
 
+    console.log('invoked');
     let body =event.body;
     let greeting = `${body.salutation} ${body.name}`;
     let headers = {};
     headers['Content-Type'] = 'application/json'
 
+    console.log(`greeting is ${greeting}`);
+
     return {
-        'statusCode': 200,
-        'headers': headers,
-        'body': {greeting:greeting}
+        statusCode: 200,
+//        headers: headers,
+        body: {greeting:greeting},
+        isBase64Encoded: false
     };
 }
 
