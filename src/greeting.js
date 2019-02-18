e9e43948d042b7660d6c272f4b0499b3b993a710
d-smith <doug.smith.mail@gmail.com>
2019-02-18 09:34:47 -0800

    Added error mapping

--- src/greeting.js
+++ src/greeting.js
@@ -3,14 +3,15 @@ const greet = async (event, context) => {
     console.log('invoked');
     let body =event.body;
     let greeting = `${body.salutation} ${body.name}`;
-    let headers = {};
-    headers['Content-Type'] = 'application/json'
 
     console.log(`greeting is ${greeting}`);
 
+    if(greeting == 'hello world') {
+        throw new Error('TiredGreeting');
+    }
+
     return {
         statusCode: 200,
-//        headers: headers,
         body: {greeting:greeting},
         isBase64Encoded: false
     };
